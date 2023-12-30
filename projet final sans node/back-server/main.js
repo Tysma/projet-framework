bffafd317618ced4714c6566902dbcf7107a0571
Mathys Caron <77667297+Tysma@users.noreply.github.com>
2023-12-30 21:00:31 +0100

    Add files via upload

---
+++ projet final sans node/back-server/main.js
@@ -0,0 +1,1 @@
+console.log(process.env.NVM_DIR="$HOME/.nvm")
---
+++ projet final sans node/front-client/src/components/etatConnexion.ts
@@ -0,0 +1,5 @@
+// sharedState.ts
+import { ref } from 'vue';
+
+export const isConnected = ref(false);
+export const userId = ref(null); // Initialisez l'ID utilisateur à null
